c972d955f5cf88abcd97029b55d60c15c65c2024
pari kannappan <parikannappan@gmail.com>
2024-06-19 17:58:54 +0530

    Add files via upload

--- Bankrptup.py
+++ Bankrptup.py
@@ -6,8 +6,7 @@ Created on Sat Jun 15 06:12:15 2024
 """
 
 import pandas as pd
-import streamlit as st
-import numpy as np 
+import streamlit as st 
 st.set_page_config(page_title="Bank Statement Search", page_icon="bank", layout="wide")
 st.title("  :bank: :blue[Bank Statement Search App]")
 fl = st.file_uploader(" Upload a file using 'Browse files' ", type=(["csv"]))
@@ -18,24 +17,29 @@ if fl is not None:
         Bankdata = pd.read_csv(fl, header=22)
         return Bankdata
     Bankdata = load_data(fl)
-    Bankdata['   Withdrawals ']= pd.to_numeric(Bankdata['   Withdrawals '], errors='coerce') 
-    Bankdata['   Balance   ']= pd.to_numeric(Bankdata['   Balance   '], errors='coerce')
-    Bankdata[' Deposits ']= pd.to_numeric(Bankdata[' Deposits '],  errors='coerce')
+    Bankdata.columns = [col.strip() for col in Bankdata.columns]
+    Bankdata['Withdrawals']= pd.to_numeric(Bankdata['Withdrawals'], errors='coerce') 
+    Bankdata['Balance']= pd.to_numeric(Bankdata['Balance'], errors='coerce')
+    Bankdata['Deposits']= pd.to_numeric(Bankdata['Deposits'],  errors='coerce')
     Bankdata = Bankdata.drop('Unnamed: 5', axis=1)
 
     stinput = st.text_input("Enter keyword to search -")
-    #st.write(f':money_with_wings: :red[WITHDRAWALS  -:    {dfs1w}                ]')
-    #st.write(f':moneybag: :green[DEPOSITS   -:   {dfs1d}                 ]')
+    #
     if len(stinput) > 0:
        print('stinput-1', stinput)
        #print(np.isnan(stinput))
-       dfs1 = Bankdata.loc[Bankdata['   Description   '].str.contains(stinput, case=False)]
-       dfs1w = dfs1['   Withdrawals '].sum()
-       dfs1d = dfs1[' Deposits '].sum()
-       st.write(f':money_with_wings: :red[WITHDRAWALS  -:    {dfs1w}                ]')
-       st.write(f':moneybag: :green[DEPOSITS   -:   {dfs1d}                 ]')
+       dfs1 = Bankdata.loc[Bankdata['Description'].str.contains(stinput, case=False)]
+       def functrunc(description):
+           templst = list(description.split(' '))
+           return templst[7:10]
+       dfs1['Desc'] = dfs1['Description'].apply(functrunc) #adding a column for description
+       dfs1w = dfs1['Withdrawals'].sum()
+       dfs1d = dfs1['Deposits'].sum()
+       sumdif = dfs1w - dfs1d
+       st.write(f':money_with_wings: :red[WITHDRAWALS  -:  {dfs1w}]   :moneybag: :green[DEPOSITS   -:  {dfs1d}  ]')
+       st.write(f':abacus: With - Dep = {sumdif}                 ')
        with st.expander("View Transactions"):
-          st.dataframe(dfs1)    
+          st.dataframe(dfs1[['Date','Desc', 'Withdrawals', 'Deposits','Description']])    
     else:
         print('stinput-2 ', stinput)
         dfs1d = 0
