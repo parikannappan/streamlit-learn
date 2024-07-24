d851b3e3ad649b467d5de10404754b19b42cfe47
pari kannappan <parikannappan@gmail.com>
2024-07-24 16:30:24 +0530

    Add files via upload

--- anjanaya.py
+++ anjanaya.py
@@ -1,7 +1,9 @@
 import streamlit as st
 st.set_page_config(page_title="Temple Donnatikons", page_icon="temple", layout="wide")
-st.image('anjanayaspk.jpg', caption='The Anjaneyar Sannidhi Rennovation Donation Requirnent')
-st.write("The Anjaneyar Sannidhi opposite Sri Padalatri Narasimha Perumal temple, SP Koil is a well renowned and older temple dedicated to Anjaneya Swami",
+st.title("  :temple: :blue[The Anjaneyar Sannidhi Rennovation Donation Requirnent ]")
+st.image('anjanayaspk.jpg', caption='Sri Hanuman Thirukovil')
+st.markdown(" " %"https://g.co/kgs/Txd2fJH" )
+st.write("*The Anjaneyar Sannidhi opposite Sri Padalatri Narasimha Perumal temple, SP Koil is a well renowned and older temple dedicated to Anjaneya Swami",
          "Anjaneya in Vainava terms called Siriya Thiruvadi will always be facing the main deity. Hence in our place also, this temple is directly facing the main Narasimhar Temple", 
          "The premises around the temple had grown immensely and also the Panchayat road leading to it was elevated making the structure of the temple to go down.",
-        "Due to this, whenever it rains or excess water from other nearby lakes overflows it goes into the temple. Hence the donation is for elevating the temple structure and doing a Kumbabishekam" )
+        "Due to this, whenever it rains or excess water from other nearby lakes overflows it goes into the temple. Hence the donation is for elevating the temple structure and doing a Kumbabishekam*" )
