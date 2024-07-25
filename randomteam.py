94f4ae57bcd98acf3cfc1af0c60f4e848b7b6c6f
pari kannappan <parikannappan@gmail.com>
2024-07-25 11:32:26 +0000

    corrections

--- randomteam.py
+++ randomteam.py
@@ -19,7 +19,7 @@ with col2:
 
 players = [text1, text2, text3, text4, text5,
            text6, text7, text8, text9, text10]
-
+print(players)
 # Shuffle the list to ensure randomness
 random.shuffle(players)
 
