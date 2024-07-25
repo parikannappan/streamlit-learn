e92df7cedea2d730619ea2bcd20071fe8c000974
pari kannappan <parikannappan@gmail.com>
2024-07-25 10:00:18 +0530

    Add files via upload

--- anjanaya.py
+++ anjanaya.py
@@ -2,8 +2,9 @@ import streamlit as st
 st.set_page_config(page_title="Temple Donnations", page_icon="temple", layout="wide")
 st.title("  :hindu_temple: :blue[The Anjaneyar Sannidhi Rennovation Donation Requirment ]")
 st.image('anjanayaspk.jpg', caption='Sri Hanuman Thirukovil')
-st.markdown("https://g.co/kgs/Txd2fJH" )
+st.markdown("Location :""https://g.co/kgs/Txd2fJH" )
 st.write("*The Anjaneyar Sannidhi opposite Sri Padalatri Narasimha Perumal temple, SP Koil is a well renowned and older temple dedicated to Anjaneya Swami",
          "Anjaneya in Vainava terms called Siriya Thiruvadi will always be facing the main deity. Hence in our place also, this temple is directly facing the main Narasimhar Temple", 
          "The premises around the temple had grown immensely and also the Panchayat road leading to it was elevated making the structure of the temple to go down.",
         "Due to this, whenever it rains or excess water from other nearby lakes overflows it goes into the temple. Hence the donation is for elevating the temple structure and doing a Kumbabishekam*" )
+st.write("Trust Name: *Sannidhi Street Friends Trust*")
