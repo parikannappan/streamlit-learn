a6c9cfc1df947bf367beb40d74df402b90662a59
pari kannappan <parikannappan@gmail.com>
2024-07-25 12:22:29 +0000

    debug

--- randomteam.py
+++ randomteam.py
@@ -8,15 +8,15 @@ with col1:
       text1 = st.text_input("Player 1")
       print('text1 -', text1)
       text2 = st.text_input("Player 2")
-      text3 = st.text_input("Player 3", key="player_3")
-      text4 = st.text_input("Player 4", key = "player_4")
-      text5 = st.text_input("Player 5", key="player_5")
+      #text3 = st.text_input("Player 3", key="player_3")
+      #text4 = st.text_input("Player 4", key = "player_4")
+      #text5 = st.text_input("Player 5", key="player_5")
 with col2: 
       text6 = st.text_input("Player 6", key = "player_6")
       text7 = st.text_input("Player 7", key="player_7")
-      text8 = st.text_input("Player 8", key = "player_8")
-      text9 = st.text_input("Player 9", key="player_9")
-      text10 = st.text_input("Player 10", key = "player_10")
+      #text8 = st.text_input("Player 8", key = "player_8")
+      #text9 = st.text_input("Player 9", key="player_9")
+      #text10 = st.text_input("Player 10", key = "player_10")
 
 players = [text1, text2, text3, text4, text5,
            text6, text7, text8, text9, text10]
