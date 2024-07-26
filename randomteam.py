d49d6b4341e674d4350692ee01a93767e6caec14
pari kannappan <parikannappan@gmail.com>
2024-07-26 02:41:35 +0000

    fixing bgs

--- randomteam.py
+++ randomteam.py
@@ -27,10 +27,11 @@ print(players)
 random.shuffle(players)
 
 # Create 5 teams with 2 players each
-if len(text10) > 0:
+if len(text7) > 0:
     teams = [players[i:i + 2] for i in range(0, len(players), 2)]
+    print(f'{teams=}')
     #teams = [players[i:i + 1] for i in range(0, len(players), 2)]
 
     # Print the teams
     for idx, team in enumerate(teams, 1):
-     st.write("Team {idx}: {team}")
+     st.write("Team", {idx}, {team})
