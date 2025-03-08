46b47c422fba96a9eeb2e2b6d5c460550423a329
pari kannappan <parikannappan@gmail.com>
2025-03-09 04:27:22 +0530

    Update badtorn12.py

--- badtorn12.py
+++ badtorn12.py
@@ -2,6 +2,6 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 url = "https://docs.google.com/spreadsheets/d/1-eEqWBsgVf2O-z2sjsrV5pKeU83z6qdo2szDlkLI3TU/edit?usp=sharing"
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-data = conn.read(spreadsheet=url, usercols=list(range(10)))
+conn = st.connection("gsheets", type=GSheetsConnection)
+data = conn.read(spreadsheet=url, usercols=list(range(8)))
 st.dataframe(data)
