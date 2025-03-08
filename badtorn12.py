ec95a1ffe54c93494408405604278fe9ec02aa89
pari kannappan <parikannappan@gmail.com>
2025-03-08 19:38:15 +0000

    save code

---
+++ badtorn12.py
@@ -0,0 +1,7 @@
+import streamlit as streamlit
+from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1-eEqWBsgVf2O-z2sjsrV5pKeU83z6qdo2szDlkLI3TU/edit?usp=sharing"
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+data = conn.read(spreadsheet=url, usercols=list(range(10)))
+st.dataframe(data)
